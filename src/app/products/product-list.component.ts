d1cc445285c314b8cc0ee85869bcb9859885f319
balasekar <mailtobala89@gmail.com>
2018-12-19 17:23:08 +0000

    Directives ngIf & ngFor of

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,9 +1,31 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'pm-products',
+  selector: 'app-products',
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
-  pageTitle: string = 'Product List';
+  pageTitle = 'Product List';
+  products: any[] = [
+    {
+      'productId': 1,
+      'productName': 'Garden Cart',
+      'productCode': 'GDN-0023',
+      'releaseDate': 'December 19, 2018',
+      'description': '15 Gallon capacity rolling garden cart',
+      'price': 32.99,
+      'rating': 4.2,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+    },
+    {
+      'productId': 2,
+      'productName': 'Hammer',
+      'productCode': 'TBX-0048',
+      'releaseDate': 'December 19, 2018',
+      'description': 'Curved claw steel hammer',
+      'price': 9.99,
+      'rating': 4.8,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
+    }
+  ];
 }
