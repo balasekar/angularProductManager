85ff611a214d8a740931c3d65c4471030b49b463
egnabal <balasubramanian.gnana.sekar@ericsson.com>
2018-12-20 11:30:28 +0000

    Step: Using lifecycle Hooks

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from './product';
 
 @Component({
@@ -6,7 +6,7 @@ import {IProduct} from './product';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.sass']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 2;
@@ -37,5 +37,9 @@ export class ProductListComponent {
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('Product Component On Init');
   }
 }
