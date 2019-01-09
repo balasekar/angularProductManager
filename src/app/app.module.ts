20b6f2145ea9c0d420aac3824581b8cb5c70e133
egnabal <balasubramanian.gnana.sekar@ericsson.com>
2019-01-09 12:28:16 +0000

    Step: Building custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
 import {FormsModule} from '@angular/forms';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
