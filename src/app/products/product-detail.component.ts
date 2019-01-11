e5cd5c85fce5e0ef35b9015b941653a469dcfa13
egnabal <balasubramanian.gnana.sekar@ericsson.com>
2019-01-11 15:31:15 +0000

    Step: Activate route with code

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduct} from './product';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,9 +12,25 @@ export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product =     {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'rating': 3.2,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
